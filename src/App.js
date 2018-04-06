5a18185dfa6de8cd46106a3a9eaf1b74b32faff3
Sebastian Arboleda <SArboleda@arrivelogistics.com>
2018-04-05 22:16:31 -0500

    exercise solved

--- src/App.js
+++ src/App.js
@@ -5,17 +5,42 @@ import posts from './posts'
 // Modifica el componente App para implmentar la funcionalidad requerida
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      filteredPosts: posts,
+      value: ''
+    };
+    this.filter = this.filter.bind(this);
+  }
+  filter = (event) =>
+  {
+    this.setState(
+      {
+        value:event.target.value,
+        filteredPosts: posts.filter(x => 
+          x.title.toUpperCase().includes(event.target.value.toUpperCase()) > 0)
+      })
+  }
   render() {
+    const {filteredPosts,filter} = this.state;
     return (
       <div>
         <div className="filter">
-          <input type="text" placeholder="Ingresa el término de búsqueda" />
+          <input type="text" value={filter} onChange={this.filter} placeholder="Ingresa el término de búsqueda" />
         </div>
         <ul>
-          <li>
-            <a href={posts[0].url}><img src={posts[0].image } /></a>
-            <p>{ posts[0].title }</p>
-          </li>
+          {filteredPosts.map(
+            (element, index) => 
+            {
+              return(            
+                <li key={index}>
+                  <a href={element.url}><img alt={element.title} src={element.image} /></a>
+                  <p>{ element.title }</p>
+                </li>
+              )
+            }
+          )}
         </ul>
       </div>
     )
